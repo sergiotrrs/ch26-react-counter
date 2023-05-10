3dcafcb014a7c3b0f5fe0d789d0dfe497d2fd19a
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-10 14:26:55 -0600

    refactor: uso de Link

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const routesApp = [
+  { name: "Home", url: "/" },
+  { name: "Social Networks", url: "/social-networks" },
+  { name: "Counter +5", url: "/counter/1" },
+  { name: "Counter +10", url: "/counter/2" },
+];
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      <ul>
+        {routesApp.map((data) => (
+          <li key={data.url}>
+            <Link  to={data.url} >{data.name}</Link>
+            {/* <a href={data.url}> {data.name} </a> */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
