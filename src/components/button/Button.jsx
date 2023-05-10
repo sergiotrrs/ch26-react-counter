3d7bb6c84e9db4d3d09d2a714133c968ec14878c
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-10 12:53:31 -0600

    refactor: setcounter en Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Button = ({ onClick, children }) => {
+export const Button = ({ children, setcounter, newValue }) => {
+
+    const handleOnClick = (event)=>{       
+        setcounter(  newValue  ) ;       
+    }
+
   return (
     <>
-      <button style={{ color: "blue" }} onClick={onClick}>
+      <button style={{ color: "blue" }} onClick={ handleOnClick  }>
         {children}
       </button>
     </>
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
 import { Button } from "../button/Button";
 
-
 export const Counter = ( {initialValue=0, increment=1, decrement=1} ) => {
    
     const [counter, setcounter] = useState( initialValue );
- 
-    const handleIncrement = (event)=>{       
-        setcounter( counter + increment )        
-    }
- 
-    const handleDecrement = (event)=>{       
-        setcounter( counter - decrement )        
-    }
- 
-    const handleReset = (event)=>{       
-        setcounter( initialValue ) ;       
-    }
-
 
   return (
      <>
         <h1>Counter</h1>        
         <h2>{ counter }</h2>
-        <Button onClick= { handleIncrement  } >+</Button>
-        <Button onClick= { handleDecrement  } >-</Button>
-        <Button onClick= { handleReset  } >Reset</Button>
-
+        <Button setcounter= { setcounter  } newValue={ counter + increment } >+</Button>
+        <Button setcounter= { setcounter  } newValue={ counter - decrement }  >-</Button>
+        <Button setcounter= { setcounter  } newValue={ initialValue }  >Reset</Button>
      </>   
   );
 };
