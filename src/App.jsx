6116d33ff9b99e55b8f435468df190afaa63f8dc
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-10 14:04:12 -0600

    feat: uso de react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { SocialNetworks } from "./components/socialNetworks/SocialNetworks";
 import { NavBar } from "./components/navBar/NavBar";
 import { Footer } from "./components/footer/Footer";
 import { Counter } from "./components/counter/Counter";
+import { Home } from "./components/home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/notfound/NotFound";
 
 export const App = () => {
   return (
     <>
       <NavBar />
-      <SocialNetworks />
-      <Counter initialValue={100} increment={5} decrement={5} /> 
-      <Counter initialValue={0} increment={10}/> 
+
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="social-networks" element={ <SocialNetworks /> } />
+        <Route path="counter/1" element={ <Counter initialValue={100} increment={5} decrement={5} /> } />
+        <Route path="counter/2" element={ <Counter initialValue={0} increment={10} /> } />                                
+        <Route path="*" element={  <NotFound /> } />
+        {/* <Route path='*' element={<Navigate to='/counter/1' />} />   */}                      
+      </Routes>
+
       <Footer />
     </>
   );
