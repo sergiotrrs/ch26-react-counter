ebe1441926be8547650b537e47e752ffc702cffa
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-10 10:40:11 -0600

    ci: funcion onClick

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,12 +3,17 @@ import React from "react";
 export const Counter = () => {
     let counter = 100;
 
+    const handleIncrement = (event)=>{
+        console.log(event);
+        console.log(++counter);
+    }
+
   return (
      <>
-        <h1>Counter</h1>
+        <h1>Counter</h1>        
         <h2>{ counter }</h2>
-        <button>+</button>
-        <button>-</button>
+        <button onClick= { handleIncrement  } >+</button>
+        <button onClick= { ( event )=>console.log(event) } >-</button>
         <button>Reset</button>
 
      </>   
