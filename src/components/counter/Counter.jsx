b9c533791a7e23187b5d583df20831b33ff1ffe8
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-10 11:14:17 -0600

    ci: counter decrement y reset

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Counter = () => {
-    let counter = 100;
-
-    const handleIncrement = (event)=>{
-        console.log(event);
-        console.log(++counter);
+   
+    const [counter, setcounter] = useState( 99 );
+ 
+    const handleIncrement = (event)=>{       
+        setcounter( counter + 1 )        
     }
+ 
+    const handleDecrement = (event)=>{       
+        setcounter( counter - 1 )        
+    }
+ 
+    const handleReset = (event)=>{       
+        setcounter( 0 )        
+    }
+
 
   return (
      <>
         <h1>Counter</h1>        
         <h2>{ counter }</h2>
         <button onClick= { handleIncrement  } >+</button>
-        <button onClick= { ( event )=>console.log(event) } >-</button>
-        <button>Reset</button>
+        <button onClick= { handleDecrement  } >-</button>
+        <button onClick= { handleReset  } >Reset</button>
 
      </>   
   );
