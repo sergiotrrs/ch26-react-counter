45c652fb4fc0bffed1d23152da4ae88e316451fb
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-11 11:07:52 -0600

    ci: typography Material ui

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,7 +3,8 @@ import { Button } from "../button/Button";
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import { ButtonGroup } from "@mui/material";
+import { ButtonGroup, Typography } from "@mui/material";
+import { purple, red } from '@mui/material/colors';
 
 export const Counter = ( {initialValue=0, increment=1, decrement=1} ) => {
    
@@ -11,8 +12,8 @@ export const Counter = ( {initialValue=0, increment=1, decrement=1} ) => {
 
   return (
      <>
-        <h1>Counter</h1>        
-        <h2>{ counter }</h2>
+        <Typography color={purple[700]}variant="h1" align="center">Counter</Typography>
+        <Typography color={purple[700]} variant="h2" align="center">{ counter }</Typography>     
         <ButtonGroup variant="contained" aria-label="outlined primary button group">
         <Button startIcon={ <AddCircleIcon /> } setcounter= { setcounter  } newValue={ counter + increment } ></Button>
         <Button startIcon={ <RemoveCircleIcon /> } setcounter= { setcounter  } newValue={ counter - decrement }  ></Button>
